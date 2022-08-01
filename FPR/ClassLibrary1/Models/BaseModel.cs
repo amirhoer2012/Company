a259ab5e8c09ed2bde7a9846f07d01f6d590adeb
amirhoer2012 <45909161+amirhoer2012@users.noreply.github.com>
2022-08-01 10:46:05 +0430

    StartPoint
    without Models

---
+++ FPR/ClassLibrary1/Models/BaseModel.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+   public class BaseModel
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Code { get; set; }
+        public DateTime DateGenerated { get; set; }
+        public DateTime DateModified { get; set; }
+        [ForeignKey("ModifierUser")]
+        public Guid? ModifierUserID { get; set; }
+        public User ModifierUser { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ FPR/ClassLibrary1/Models/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models.Users
+{
+   public class User:BaseModel
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+
+    }
+}
